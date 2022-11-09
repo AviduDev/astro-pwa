28172a3c01ac2d1323ba26ad861bac9327792644
AviduDev <avidudev@gmail.com>
2022-11-09 12:28:33 +0530

    site map and robot txt packages added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,18 @@ import { defineConfig } from 'astro/config';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+import robotsTxt from 'astro-robots-txt';
+
+// https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [VitePWA()],
-	},
+  vite: {
+    plugins: [VitePWA()]
+  },
+  site: 'https://avidu.me',
+  integrations: [sitemap(), prefetch()] [robotsTxt()],
 });
